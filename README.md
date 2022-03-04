2ececdedc357de00c914511f5ac0ba650f2ff8e3
Miles Kilgas <71590164+sensoorr@users.noreply.github.com>
2022-03-04 14:05:56 +0000

    Better code lol

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 [![Run on Repl.it](https://repl.it/badge/github/sensoorr/kahoot-bot)](https://repl.it/github/sensoorr/kahoot-bot)
 
-# Kahoot-bot
+# Kahoot Bot
 A simple bot that joins a kahoot game and answers with random answers.
 
 1. This project requires node.js v6 or higher. If you do not have it, you can install it from http://nodejs.org
@@ -16,5 +16,5 @@ If you want the bot to join a Kahoot lobby, do the following:
 1. Run `node index.js` in CMD.
 2. It will ask for the Game ID. Once entered, the bot will join.
 
-**Customzing**
-You can change the names of the bots in the settings.json file.
+**Config**
+Changing the bot's names, randomizing names, Game ID and amount of bots is available to do in the settings.json file.
--- index.js
+++ index.js
@@ -2,21 +2,29 @@ const Kahoot = require("kahoot.js-updated");
 const settings = require('./settings.json');
 const Chance = require('chance');
 const chance = new Chance();
-const readline = require("readline");
-const readlines = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+const pin = settings.gameID;
 
-readlines.question("GameID: ", (answer) => {
-    var splits = answer.split(",");
-    const pin = splits[0];
-        const client = new Kahoot();
-        const name = settings.random_name ? chance.name() : settings.bot_name+i;
-        client.join(pin, name);
-        console.log(`I have logged in as ${name}`);
-        client.on("questionStart", (question) => {
-            question.answer(Math.floor(Math.random() * 4));
-        });
-        return client;
-});
+function joinKahoot() {
+    const client = new Kahoot;
+    const name = settings.random_name ? chance.name() : settings.bot_name+i;
+
+    client.join(pin, name);
+    
+    client.on("Joined", () => {
+        console.log(`Logged in as ${name}`);
+    }); 
+    
+    client.on("QuizStart", () => {
+        console.log("The Kahoot has started.")
+    });
+    
+    client.on("QuestionStart", question => {
+        question.answer(Math.floor(Math.random() * 2));
+    });
+
+    client.on("QuizEnd", () => {
+        console.log("The Kahoot has ended.");
+    }); 
+};
+
+joinKahoot()
